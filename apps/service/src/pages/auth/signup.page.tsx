61db173d0faea40d1cb9a2c0baa78616438e0ee0
Alang Kim <rlaangh77@gmail.com>
2025-03-28 02:10:50 +0900

    feat: SignUp 페이지 UI 퍼블리싱

--- apps/service/src/pages/auth/signup.page.tsx
+++ apps/service/src/pages/auth/signup.page.tsx
@@ -1,46 +1,21 @@
+import { PageLayout } from "@/shared/layouts/page-layout";
+import { SignupForm } from "@/widgets/auth/signup-form";
+import { Logo } from "@/widgets/logo";
+
 export function SignupPage() {
   return (
-    <>
-      <h1>DeMentor: Service</h1>
-      <h2>회원가입</h2>
-      <div className="max-w-md mx-auto">
-        <form className="space-y-4">
-          <div>
-            <label className="block text-sm font-medium">이름</label>
-            <input
-              type="text"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium">이메일</label>
-            <input
-              type="email"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium">비밀번호</label>
-            <input
-              type="password"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium">비밀번호 확인</label>
-            <input
-              type="password"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
-          >
-            가입하기
-          </button>
-        </form>
+    <PageLayout className="flex flex-col items-center justify-center">
+      <div className="space-y-4 w-full px-4 max-w-xl">
+        <div>
+          <Logo className="text-center" />
+        </div>
+        <SignupForm
+          onValidateNameDuplicate={() => Promise.resolve()}
+          onValidateEmailDuplicate={() => Promise.resolve()}
+          onValidateEmailAuth={() => Promise.resolve()}
+          onSubmit={() => {}}
+        />
       </div>
-    </>
+    </PageLayout>
   );
 }
