fd13c218bf570d484d69ed3e72489f261d745cf7
Alang Kim <rlaangh77@gmail.com>
2025-03-28 00:44:25 +0900

    test: 테스트 코드 추가

--- apps/service/test/pages/home.page.test.tsx
+++ apps/service/test/pages/home.page.test.tsx
@@ -1,5 +1,5 @@
-import { cleanup, fireEvent, within } from "@testing-library/react";
-import { afterEach, expect, test, vi } from "vitest";
+import { fireEvent, within } from "@testing-library/react";
+import { expect, test, vi } from "vitest";
 
 import { HomePage } from "@/pages";
 import { ROUTE_PATH } from "@app/routes";
@@ -23,11 +23,6 @@ const Stub = createRoutesStub([
     Component: MockApplyMentorPage,
   },
 ]);
-
-afterEach(() => {
-  cleanup();
-  vi.clearAllMocks();
-});
 
 test("화면상에 개발자 멘토링 플랫폼이 나타난다.", () => {
   // arrange
