26586cfcb369cd0cd235b925d347a03d13519f41
Alang Kim <rlaangh77@gmail.com>
2025-03-27 17:46:00 +0900

    config: [service] feature sliced design 패턴 적용

--- apps/service/src/main.tsx
+++ apps/service/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 
-import { router } from "@/routes/router";
+import { router } from "@/app/routes/router";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
