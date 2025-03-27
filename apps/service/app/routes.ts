c91786033c42921ce91899e3a29e5c01b2165cf6
Alang Kim <rlaangh77@gmail.com>
2025-03-27 19:15:51 +0900

    feat: [service] 페이지 라우팅 설정

--- apps/service/app/routes.ts
+++ apps/service/app/routes.ts
@@ -1,7 +1,30 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
+export const ROUTE_PATH = {
+  HOME: "/",
+  AUTH: {
+    LOGIN: "/auth/login",
+    SIGNUP: "/auth/signup",
+    SIGNUP_COMPLETE: "/auth/signup-complete",
+  },
+  MENTOR: {
+    DASHBOARD: "/mentor/dashboard",
+  },
+  APPLY_MENTOR: "/apply-mentor",
+  AVAILABLE_CLASSES: "/available-classes",
+  MY: "/my",
+};
+
 export default [
   index("routes/home.tsx"),
-  route("about", "routes/about.tsx"),
-  route("contact", "routes/contact.tsx"),
+
+  route(ROUTE_PATH.AUTH.LOGIN, "routes/auth/login.tsx"),
+  route(ROUTE_PATH.AUTH.SIGNUP, "routes/auth/signup.tsx"),
+  route(ROUTE_PATH.AUTH.SIGNUP_COMPLETE, "routes/auth/signup-complete.tsx"),
+
+  route(ROUTE_PATH.MENTOR.DASHBOARD, "routes/mentor/dashboard.tsx"),
+
+  route(ROUTE_PATH.APPLY_MENTOR, "routes/apply-mentor.tsx"),
+  route(ROUTE_PATH.AVAILABLE_CLASSES, "routes/available-classes.tsx"),
+  route(ROUTE_PATH.MY, "routes/my.tsx"),
 ] satisfies RouteConfig;
