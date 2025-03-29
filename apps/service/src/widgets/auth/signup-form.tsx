a3f547b36cb04adf9b374399df1e2f427b36d4f9
Alang Kim <rlaangh77@gmail.com>
2025-03-29 17:42:59 +0900

    fix: 이메일 입력하지 않았을때 표시문구가 잘못된 문제 수정

--- apps/service/src/widgets/auth/signup-form.tsx
+++ apps/service/src/widgets/auth/signup-form.tsx
@@ -20,11 +20,11 @@ const signupFormSchema = z
   .object({
     email: z
       .string()
-      .email({
-        message: "이메일 형식이 올바르지 않습니다.",
-      })
       .nonempty({
         message: "이메일을 입력해주세요.",
+      })
+      .email({
+        message: "이메일 형식이 올바르지 않습니다.",
       }),
     password: z
       .string()
