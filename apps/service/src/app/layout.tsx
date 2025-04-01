1c230be21bac24651b5b5605436b890ce0a267d6
Alang Kim <rlaangh77@gmail.com>
2025-04-01 16:17:54 +0900

    config: react-query 설정

--- apps/service/src/app/layout.tsx
+++ apps/service/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Links, Meta, Scripts, ScrollRestoration } from "react-router";
 
 import { Route } from ".react-router/types/app/+types/root";
@@ -21,26 +22,33 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
+const queryClient = new QueryClient();
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider defaultTheme="dark">
-      <html lang="en">
-        <head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <Meta />
-          <Links />
-        </head>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark">
+        <html lang="en">
+          <head>
+            <meta charSet="utf-8" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+            <Meta />
+            <Links />
+          </head>
 
-        <body className="min-h-dvh flex flex-col">
-          <NavigationHeader />
-          <div className="flex-1 flex flex-col">{children}</div>
-          <Footer />
-          <PageNavigationDevtools />
-          <ScrollRestoration />
-          <Scripts />
-        </body>
-      </html>
-    </ThemeProvider>
+          <body className="min-h-dvh flex flex-col">
+            <NavigationHeader />
+            <div className="flex-1 flex flex-col">{children}</div>
+            <Footer />
+            <PageNavigationDevtools />
+            <ScrollRestoration />
+            <Scripts />
+          </body>
+        </html>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
