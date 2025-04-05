d1521db5deba6296ccfa8c1964e3c8229a2fa958
Alang Kim <rlaangh77@gmail.com>
2025-04-06 00:50:34 +0900

    fix: 비밀번호 확인이 동작하지 않는 문제 수정

--- apps/service/src/widgets/auth/signup-form.tsx
+++ apps/service/src/widgets/auth/signup-form.tsx
@@ -1,3 +1,4 @@
+import { handleError } from "@/app/error-handler/error-handler";
 import { DuplicateError } from "@/app/errors/duplicate.error";
 import { VerifyEmailModal } from "@/widgets/auth/verify-email-modal";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -19,58 +20,48 @@ import { useEffect, useMemo, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-const signupFormSchema = z
-  .object({
-    email: z
-      .string()
-      .nonempty({
-        message: "이메일을 입력해주세요.",
-      })
-      .email({
-        message: "이메일 형식이 올바르지 않습니다.",
-      }),
-    emailDuplicateChecked: z.boolean().refine((val) => val === true, {
-      message: "이메일 중복 확인이 필요합니다.",
+const signupFormSchema = z.object({
+  email: z
+    .string()
+    .nonempty({
+      message: "이메일을 입력해주세요.",
+    })
+    .email({
+      message: "이메일 형식이 올바르지 않습니다.",
     }),
-    verifyCode: z.string().min(1, {
-      message: "이메일 코드를 인증해주세요.",
+  emailDuplicateChecked: z.boolean().refine((val) => val === true, {
+    message: "이메일 중복 확인이 필요합니다.",
+  }),
+  verifyCode: z.string().min(1, {
+    message: "이메일 코드를 인증해주세요.",
+  }),
+  password: z
+    .string()
+    .nonempty({
+      message: "비밀번호를 입력해주세요.",
+    })
+    .min(8, {
+      message: "비밀번호는 8자 이상이어야 합니다.",
+    })
+    .max(16, {
+      message: "비밀번호는 16자 이하여야 합니다.",
+    })
+    .regex(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
+      message: "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.",
     }),
-    password: z
-      .string()
-      .nonempty({
-        message: "비밀번호를 입력해주세요.",
-      })
-      .min(8, {
-        message: "비밀번호는 8자 이상이어야 합니다.",
-      })
-      .max(16, {
-        message: "비밀번호는 16자 이하여야 합니다.",
-      })
-      .regex(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
-        message: "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.",
-      }),
-    confirmPassword: z.string().min(1, {
-      message: "비밀번호 확인을 입력해주세요.",
-    }),
-    name: z.string().min(1, {
-      message: "이름을 입력해주세요.",
-    }),
-    nickname: z.string().min(1, {
-      message: "닉네임을 입력해주세요.",
-    }),
-    nicknameDuplicateChecked: z.boolean().refine((val) => val === true, {
-      message: "닉네임 중복 확인이 필요합니다.",
-    }),
-  })
-  .superRefine((data, ctx) => {
-    if (data.password !== data.confirmPassword) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        path: ["confirmPassword"],
-        message: "비밀번호가 일치하지 않습니다.",
-      });
-    }
-  });
+  confirmPassword: z.string().min(1, {
+    message: "비밀번호 확인을 입력해주세요.",
+  }),
+  name: z.string().min(1, {
+    message: "이름을 입력해주세요.",
+  }),
+  nickname: z.string().min(1, {
+    message: "닉네임을 입력해주세요.",
+  }),
+  nicknameDuplicateChecked: z.boolean().refine((val) => val === true, {
+    message: "닉네임 중복 확인이 필요합니다.",
+  }),
+});
 
 export type SignupFormData = z.infer<typeof signupFormSchema>;
 
@@ -108,6 +99,19 @@ export function SignupForm({
 
   const watchedEmail = form.watch("email");
   const watchedNickname = form.watch("nickname");
+  const watchedPassword = form.watch("password");
+  const watchedConfirmPassword = form.watch("confirmPassword");
+
+  useEffect(() => {
+    if (
+      form.formState.dirtyFields.confirmPassword &&
+      watchedPassword !== watchedConfirmPassword
+    ) {
+      form.setError("confirmPassword", {
+        message: "비밀번호가 일치하지 않습니다.",
+      });
+    }
+  }, [form, watchedPassword, watchedConfirmPassword]);
 
   useEffect(() => {
     form.setValue("emailDuplicateChecked", false);
@@ -194,7 +198,7 @@ export function SignupForm({
               close(verifyCode);
             }}
             onVerifyFailure={(error) => {
-              console.error(error);
+              handleError(error);
             }}
             onValidateEmailVerifyCode={onValidateEmailVerifyCode}
           />
