346b6bf05152333ac94114ac2acae907f997f40d
Alang Kim <rlaangh77@gmail.com>
2025-04-07 13:28:39 +0900

    feat: button-with-loading props 인터페이스 수정

--- packages/ui/src/components/button-with-loading.tsx
+++ packages/ui/src/components/button-with-loading.tsx
@@ -1,24 +1,32 @@
 import { cn } from "@repo/utils/cn";
-import { forwardRef, useState } from "react";
+import { forwardRef, useMemo, useState } from "react";
 import * as reactSpinners from "react-spinners";
 import { Button } from "./button";
 
 const { BarLoader } = reactSpinners;
 
 interface Props extends React.ComponentProps<typeof Button> {
+  loading?: boolean;
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => Promise<void>;
 }
 
 export const ButtonWithLoading = forwardRef<HTMLButtonElement, Props>(
-  function Element({ children, className, onClick, disabled, ...props }, ref) {
-    const [isLoading, setIsLoading] = useState(false);
+  function Element(
+    { children, className, onClick, disabled, loading = false, ...props },
+    ref
+  ) {
+    const [internalLoading, setInternalLoading] = useState(loading);
+    const isLoading = useMemo(
+      () => internalLoading || loading,
+      [internalLoading, loading]
+    );
 
     const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
       try {
-        setIsLoading(true);
+        setInternalLoading(true);
         await onClick?.(e);
       } finally {
-        setIsLoading(false);
+        setInternalLoading(false);
       }
     };
 
