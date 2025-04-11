37b26bd58e34fc3b06e96568aab4bdbbb8ea584d
Alang Kim <rlaangh77@gmail.com>
2025-04-11 18:03:35 +0900

    feat: 직무 조회 API 연동

--- apps/service/src/pages/available-classes/hooks/use-get-job-categories.ts
+++ apps/service/src/pages/available-classes/hooks/use-get-job-categories.ts
@@ -1,35 +1,20 @@
 import { JobCategoryModel } from "@/entities/model/class/job-category.model";
 import { ModelCreator } from "@/entities/model/model-creator";
+import { dementorApiFetchers } from "@repo/api";
 import { useSuspenseQuery } from "@tanstack/react-query";
-
-const MOCK_JOB_CATEGORIES = [
-  {
-    id: "Java",
-    label: "자바",
-  },
-  {
-    id: "JavaScript",
-    label: "자바스크립트",
-  },
-  {
-    id: "Python",
-    label: "파이썬",
-  },
-  {
-    id: "C#",
-    label: "C#",
-  },
-];
 
 export function useGetJobCategories() {
   const query = useSuspenseQuery({
     queryKey: ["job-categories"],
-    queryFn: () =>
-      new Promise<typeof MOCK_JOB_CATEGORIES>((resolve) =>
-        resolve(MOCK_JOB_CATEGORIES)
-      ),
-    select: (data) =>
-      data.map((item) => ModelCreator.create(JobCategoryModel, item)),
+    queryFn: () => dementorApiFetchers.admin.getJobCategoryList({}),
+    select: (data) => {
+      return data.data?.map((jobCategory) => {
+        return ModelCreator.create(JobCategoryModel, {
+          id: jobCategory.jobId?.toString() ?? "-1",
+          label: jobCategory.name ?? "",
+        });
+      });
+    },
   });
 
   return query;
