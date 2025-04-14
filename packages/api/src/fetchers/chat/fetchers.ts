7922ab6845c34607e668ab1180219be53b6e9ee2
Alang Kim <rlaangh77@gmail.com>
2025-04-15 02:23:34 +0900

    feat: 채팅룸 관련 api 추가

--- packages/api/src/fetchers/chat/fetchers.ts
+++ packages/api/src/fetchers/chat/fetchers.ts
@@ -1,1 +1,66 @@
-export {};
+import { generateServiceFetcher } from "@/fetchers/generate-service-fetcher";
+import {
+  ChatMessageResponseDto,
+  ChatMessageSendDto,
+  ChatRoomResponseDto,
+} from "@/swagger/schemas";
+
+export const createAdminChatRoom = generateServiceFetcher<
+  void,
+  void,
+  void,
+  ChatRoomResponseDto
+>({
+  method: "POST",
+  endpoint: "/api/chat/admin-room",
+});
+
+export const getChatRoom = generateServiceFetcher<
+  { chatRoomId: number },
+  void,
+  void,
+  ChatRoomResponseDto
+>({
+  method: "GET",
+  endpoint: "/api/chat/room/{chatRoomId}",
+});
+
+export const getMemberChatRoomList = generateServiceFetcher<
+  void,
+  void,
+  void,
+  ChatRoomResponseDto[]
+>({
+  method: "GET",
+  endpoint: "/api/chat/member/rooms",
+});
+
+export const getAdminChatRoomList = generateServiceFetcher<
+  void,
+  void,
+  void,
+  ChatRoomResponseDto[]
+>({
+  method: "GET",
+  endpoint: "/api/chat/admin/rooms",
+});
+
+export const getChatRoomMessages = generateServiceFetcher<
+  { chatRoomId: number },
+  { beforeMessageId: number },
+  void,
+  ChatMessageResponseDto[]
+>({
+  method: "GET",
+  endpoint: "/api/chat/rooms/{chatRoomId}/messages",
+});
+
+export const sendMessage = generateServiceFetcher<
+  { chatRoomId: number },
+  void,
+  ChatMessageSendDto,
+  ChatMessageResponseDto
+>({
+  method: "POST",
+  endpoint: "/api/chat/rooms/{chatRoomId}/messages",
+});
